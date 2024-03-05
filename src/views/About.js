412d9f228a5ecc53794d15e53a73545e8acc1075
Joachim Karlsson <joachim.karlsson@zingtongroup.com>
2024-03-05 19:44:57 +0100

    Ändrade Bio

--- src/views/About.js
+++ src/views/About.js
@@ -4,47 +4,13 @@ import './About.css'
 const About = () => {
   return (
     <div className='about-page'>
-        <h1>Profil</h1>
+        <h1>Bio</h1>
         <div className='about-intro'>
-            <p>Kreativ och ansvarstagande kreatör med passion för design och
-kommunikation. Ett skarpt sinne för att möta kunders önskningar.
-Utbildad inom marknadsföring, visuell kommunikation, projektledning,
-hållbara affärsmodeller. Arbetat pro bono för mediaprofiler som Robin
-Berglund och Marcus Thapper. Skapat visuella uttag i form av affischer
-och omslag till podcasts och standup-produktion. Över två års erfarenhet
-inom klädesbranschen och försäljning.</p>
+            <p>Sofia Mäkiniemi is a multifaceted Swedish artist currently living in Bournemouth UK.
+                With a diverse background that spans music, art history, and curation, Mäkiniemi's artistic journey is a testament to the richness that interdisciplinary exploration can bring to the creative process. </p>
+                <p>Currently pursuing her Master's degree in Fine Art at Arts University Bournemouth, Mäkiniemi is immersed in an exploration of autobiographical art, with a particular emphasis on themes of grief and trauma. This introspective journey is unraveling the complexities of the human experience and translating them into visual narratives. Through her artistic lens, she opens a dialogue about the shared human experiences of pain, loss, and resilience. The raw honesty embedded in her creations creates a bridge between artist and audience, fostering a connection that transcends the confines of the gallery space.</p>
         </div>
-        <div className='about-info'>
-            <div>
-            <h4>Kompetenser</h4>
-                <div className='grid-container'>
-                    <div className='grid-item'>
-                        <p>Adobe CC</p>
-                        <li>Illustrator</li>
-                        <li>InDesign</li>
-                        <li>Photoshop</li>
-                    </div>
-                    <div className='grid-item'>
-                        <p>Program</p>
-                        <li>Office-paketet</li>
-                        <li>Figma</li>
-                    </div>
-                    <div className='grid-item'>
-                        <p>Språk</p>
-                        <li>Svenska</li>
-                        <li>Engelska</li>
-                        <li>Grekiska</li>
-                    </div>
-                    <div className='grid-item'>
-                        <p>Övrigt</p>
-                        <li>Service Minded</li>
-                        <li>Textilkunskaper</li>
-                        <li>Försäljning</li>
-                    </div>
-                </div>
-            </div>
-            
-        </div>
+        
     </div>
   )
 }
